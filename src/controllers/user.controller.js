59d0397d9cfc89122422c21f0e077c6a1cd0b574
Kushal Popli <kushalpopli69@gmail.com>
2025-07-31 00:24:45 +0530

    Add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,64 +31,69 @@ const registerUser = asyncHandler( async (req,res) => {
   // check for user creation
   // return res
 
-  const { fullName, email, username, password } = req.body
-  // console.log("email: ",email)
-  
-  if (
-    [fullName, email, username, password].some((field) => field?.trim() === "")
-  ) {
-    throw new ApiError(400, "All fields are required")
-  }
-  
-  const existedUser = await User.findOne({
-    $or: [{ username }, { email }]
-  })
-  
-  if(existedUser){
-    throw new ApiError(409, "User with email or username already exists")
-  }
+   const { fullName, email, username, password } = req.body
+   // console.log("email: ",email)
+   
+   if (
+     [fullName, email, username, password].some((field) => field?.trim() === "")
+   ) {
+     throw new ApiError(400, "All fields are required")
+   }
+   
+   const existedUser = await User.findOne({
+     $or: [{ username }, { email }]
+   })
+   
+   if(existedUser){
+     throw new ApiError(409, "User with email or username already exists")
+   }
+ 
+   // console.log(req.files)
+ 
+   const avatarLocalPath = req.files?.avatar[0]?.path
+   // const coverImageLocalPath = req.files?.coverImage[0]?.path
+ 
+  //  let avatarLocalPath;
+  //  if (req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0) {
+  //    avatarLocalPath = req.files.avatar[0].path;
+  //  }
 
-  // console.log(req.files)
-
-  const avatarLocalPath = req.files?.avatar[0]?.path
-  // const coverImageLocalPath = req.files?.coverImage[0]?.path
-
-  let coverImageLocalPath
-  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-    coverImageLocalPath = req.files.coverImage[0].path
-  }
-
-  if(!avatarLocalPath){
-    throw new ApiError(400, "Avatar file is required")
-  }
-  // console.log(coverImageLocalPath)
-  const avatar = await uploadOnCloudinary(avatarLocalPath)
-  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-
-  if(!avatar){
-    throw new ApiError(400, "Avatar file is required")
-  }
-// console.log(coverImage)
-  const user = await User.create({
-    fullName,
-    avatar: avatar.url,
-    coverImage: coverImage?.url || "",
-    email,
-    password,
-    username: username.toLowerCase()
-  })
-
-  const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  )
-
-  if(!createdUser){
-    throw new ApiError(500, "Something went wrong while registering the user")
-  }
-
-  return res.status(201).json(
-    new ApiResponse(200, createdUser, "User registered Successfully")
-  )
+   let coverImageLocalPath
+   if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+     coverImageLocalPath = req.files.coverImage[0].path
+   }
+ 
+   if(!avatarLocalPath){
+     throw new ApiError(400, "Avatar file is required")
+   }
+   // console.log(coverImageLocalPath)
+   const avatar = await uploadOnCloudinary(avatarLocalPath)
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+ 
+   if(!avatar){
+     throw new ApiError(400, "Avatar file is required")
+   }
+ // console.log(coverImage)
+   const user = await User.create({
+     fullName,
+     avatar: avatar.url,
+     coverImage: coverImage?.url || "",
+     email,
+     password,
+     username: username.toLowerCase()
+   })
+ 
+   const createdUser = await User.findById(user._id).select(
+     "-password -refreshToken"
+   )
+ 
+   if(!createdUser){
+     throw new ApiError(500, "Something went wrong while registering the user")
+   }
+ 
+   return res.status(201).json(
+     new ApiResponse(200, createdUser, "User registered Successfully")
+   )
 
 })
 
@@ -109,9 +114,9 @@ const loginUser = asyncHandler(async(req,res) => {
 
   // Here is an alternative of above code based on logic discussed in video:
   // if(!username || !email){
-  if(!(username || email)){
-    throw new ApiError(400,"username or email is required")
-  }
+  // if(!(username || email)){
+  //   throw new ApiError(400,"username or email is required")
+  // }
 
   const user = await User.findOne({
     $or: [{username}, {email}]
